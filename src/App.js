8ccf9d6b1f1f4451ac5f3c378618898c7ce80545
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-10 12:00:02 +0900

    AND 연산자(&&)를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,14 +2,5 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return(
-    <div>
-{ name === '리액트' ? (
-<h1> 리액트입니다.</h1>
-) : (
-<h2> 리액트가 아닙니다.</h2>
-)}
-</div>
-  );
-}
+  return <div> { name === '리액트' && <h1> 리액트입니다.</h1>} </div>}
 export default App;
