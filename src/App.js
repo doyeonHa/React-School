180a1dbda925247556cb492d49b47e5af7edc327
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-10 12:06:23 +0900

    꼭 닫아야 하는 태그 <input></input>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-return <div className="react"> {name} </div>
+return (
+<>
+<div className="react">{name}</div>
+<input></input> // 꼭 닫아야 하는 태그
+</>
+);
 }
 export default App;
