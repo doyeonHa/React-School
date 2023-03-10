c41af6ecb3dbbdeeb22c95e9aa50a630947d07e0
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-10 12:04:19 +0900

    class 대신 className

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-const style1 = {
-// background-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기밥으로 작성
-backgroundColor : 'black',
-color : 'aqua',
-fontSize : '48px', // font-size --> fontSize
-fontWeight : 'bold',
-padding : 16 // 단위를 생략하면 px로 지정된다.
-};
-return <div style={style1}>{name}</div>
+return <div className="react"> {name} </div>
 }
 export default App;
