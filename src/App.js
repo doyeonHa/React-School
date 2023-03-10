36bd4bbd80b771cfdc04467c3d62e98d05fe58cf
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-10 12:06:59 +0900

    주석

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ function App() {
   const name = '리액트';
 return (
 <>
-<div className="react">{name}</div>
-<input></input> // 꼭 닫아야 하는 태그
+{ /* 주석은 이렇게 작성합니다 */}
+<div
+className="react" // 시작태그를 여러 줄로 작성하게 된다면 주석을 작성
+>{name}
+</div>
+// 하지만 이런 주석이나
+/* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+<input />
 </>
 );
 }
