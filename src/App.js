47a266fc3b85ad2f71f11cd6ed8ea51d263ca1b0
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-10 12:00:49 +0900

    인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return <div> { name === '리액트' && <h1> 리액트입니다.</h1>} </div>}
+const style1 = {
+// background-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기밥으로 작성
+backgroundColor : 'black',
+color : 'aqua',
+fontSize : '48px', // font-size --> fontSize
+fontWeight : 'bold',
+padding : 16 // 단위를 생략하면 px로 지정된다.
+};
+return <div style={style1}>{name}</div>
+}
 export default App;
